43c8e1a6914ff9491327db9cc3491ee94321d0d0
irgidp <irgijak@gmail.com>
2020-10-15 22:19:58 +0700

    Latihan 10

---
+++ PBOIF210119059Latihan10Logika.java
@@ -0,0 +1,20 @@
+/* 
+* @author
+ * NAMA        : Irgi Dwiputra
+ * KELAS       : IF-2
+ * NIM         : 10119059
+ * Deskripsi   : Program ini berisi program untuk menampilkan logika
+ */
+package pboif2.pkg10119059.latihan10.logika;
+
+public class PBOIF210119059Latihan10Logika {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int umur = 19;
+        boolean dewasa;
+        dewasa = (umur >= 17);
+        System.out.println("Status Dewasa : " + dewasa);
+    }
+    
+}
